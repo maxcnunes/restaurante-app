e3ec2c19501c221350908ff3b770ba25024814b4
Max Claus Nunes <maxcnunes@gmail.com>
2014-06-04 09:41:52 -0300

    added script to setup windows environment

---
+++ bin/win-setup-app-restaurante.bat
@@ -0,0 +1,7 @@
+mkdir c:\restaurante-app
+
+if not exist c:\restaurante-app\enderecos.json (echo [] > c:\restaurante-app\enderecos.json)
+
+if not exist c:\restaurante-app\clientes.json (echo [] > c:\restaurante-app\clientes.json)
+
+pause
